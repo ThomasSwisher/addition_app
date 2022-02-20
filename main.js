242c60b62a51e025d382c3f3de5ef025b2449220
ThomasSwisher <thomas@structuredseo.com>
2022-02-20 10:29:46 -0700

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+var addition
+function add(){
+  addition = 
+  Number(document.getElementById('numOne').value) 
+  + 
+  Number(document.getElementById('numTwo').value);
+
+  document.getElementById('total').innerHTML = addition
+
+ console.log(addition)
+}
+
